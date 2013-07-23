95c963318c4a0bb861c722fb80efd034fb1f51c5
Michael Peterson <mikero17@gmail.com>
2013-07-23 16:58:07 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from django import models
 
 class Quote(models.model):
-  catalog_number = models.CharField(max_length=100)
+  	catalog_number = models.CharField(max_length=100)
 	item_description = models.CharField(max_length=100)
 	vendor = models.CharField(max_length=100)
 	quote_number = models.CharField(max_length=100)
