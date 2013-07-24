b16b9f67a84928cc7399bfd5b45b89ee43e4f3e4
Michael Peterson <mikero17@gmail.com>
2013-07-23 19:15:38 -0500

    Delete models.py
    Deleting before adding whole django repository

--- models.py
+++
@@ -1,9 +0,0 @@
-from django import models
-
-class Quote(models.model):
-  	catalog_number = models.CharField(max_length=100)
-	item_description = models.CharField(max_length=100)
-	vendor = models.CharField(max_length=100)
-	quote_number = models.CharField(max_length=100)
-	quote_expires = models.DateField()
-	
