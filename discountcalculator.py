485e36824a1cb5adca61598cc7a5ae47026355d7
mikerobiology <mikero17@gmail.com>
2013-07-30 20:21:32 -0500

    Added discountcalculator.py
    discountcalculator.py will do the math, but won't work with django.
    JQuery/Java needed?

---
+++ discountcalculator.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+
+prompt = '> '
+
+print "Enter the price."
+price = raw_input(prompt)
+
+print "Enter the discount(percentage)."
+discount_percent = raw_input(prompt)
+
+print "Here is the net cost after discount."
+discount = float(discount_percent)*float(price) / 100
+net_price = float(price) - float(discount)
+print ("%.2f" % net_price)
